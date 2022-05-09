92c2935349d19cbbfcff4bfa09850d291679ac69
Brandon corey <93304067+youngnug@users.noreply.github.com>
2022-05-09 12:00:15 -0400

    Update function_hw.py

--- function_hw.py
+++ function_hw.py
@@ -61,3 +61,16 @@ print(almost_there(90))
 print(almost_there(104))
 print(almost_there(150))
 print(almost_there(209))
+
+#FIND 33:
+#Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
+
+def has_33(nums):
+    for i in range(len(nums)-1):
+        if nums[i] == 3 and nums[i+1] == 3:
+            return True
+    return False
+           
+print(has_33([1, 3, 3]))
+print(has_33([1, 3, 1, 3]))
+print(has_33([3, 1, 3]))
